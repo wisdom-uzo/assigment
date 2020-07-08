dc9711fcbef75496bc0c7070e26c21c68ba3119a
wisdom-uzo <45757012+wisdom-uzo@users.noreply.github.com>
2020-07-08 11:59:25 -1100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // 1 checking for the minimum number
-const minNumber = (a,b) => Math.min(a,b)
+ const minNumber = (a,b)=>  a<b ? a : b
 console.log(minNumber(2,4))
 
 // 2 checking for the maximum number
-const maxNumber = (a,b) => Math.max(a,b)
+const maxNumber = (a,b) => a > b ? a : b
   console.log(maxNumber(2,4))
 
 // 3 checking if a number is diviible
@@ -11,20 +11,25 @@ const numDivisible = num => num % 2 === 0 ? true :false
   console.log(numDivisible(4))
 
 // 4 checking if a number is valid
-const ifValid = (num) => {
-    if(num < 1 || num < 20){
-        for(i=0; i>num ; i++){
+ const ifValid = (num) => {
+    if(num < 1 || num > 20){
+        for(i=0; i<num; i++){
            return i + 'invalid'
         }
     }else{
         return 'valid'
     }
-   
-}
+  }
 console.log(ifValid(40))
 
 // 5 getting the sum of an array
- const sumArray = (...num)=>  num.reduce((a,b) => a+b,0)
+const sumArray = (...arr) =>{
+  let sum = 0
+  for(let i = 0;i < arr.length ; i++){
+     sum += arr[i]
+  }
+  return sum
+}
   console.log(sumArray(5,8,9))
 
 // 6 checking if a number exist in an array
